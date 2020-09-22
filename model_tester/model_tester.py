0d66b5922986dac422a639040507e28c31b682db
Gabriel Gardin <gabrieltgardin@gmail.com>
2020-09-22 15:09:56 -0300

    Update requirements.txt

--- model_tester/model_tester.py
+++ model_tester/model_tester.py
@@ -13,13 +13,10 @@ import matplotlib.pyplot as plt
 MINIMAL_CONFIDENCE = 0.5
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = "../../mymodels/ssd_models/SSD_Mobilenetv1_FPN/frozen_inference_graph.pb"
+PATH_TO_CKPT = "/home/gardin/Documents/Estagio_LET/let_obj_detection/mymodels/ssd_models/SSD_Mobilenetv1_FPN/frozen_inference_graph.pb"
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = "../../mymodels/ssd_models/SSD_Mobilenetv1_FPN/label_map.pbtxt"
-
-
-TEST_IMAGE_PATHS = ["all_dataset_final/2367.jpg"]
+PATH_TO_LABELS = "/home/gardin/Documents/Estagio_LET/let_obj_detection/mymodels/ssd_models/SSD_Mobilenetv1_FPN/label_map.pbtxt"
 
 num_classes = 5
 
